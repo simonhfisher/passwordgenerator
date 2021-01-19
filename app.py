f69f6fb15c3e533aa30eb3a6e293f2e7d9a255c9
simonhfisher <simonhfisher@gmail.com>
2021-01-19 22:47:28 +0000

    added length_generator from challenge 16, finished password_generator

--- app.py
+++ app.py
@@ -15,14 +15,37 @@ def text_grabber(file_name):
 
     return word_list
 
+def password_generator(word_list):
+    length_of_word_list = len(word_list)
+    random_number = random.randrange(length_of_word_list)
+    random_word = word_list[random_number]
 
 
- 
+    return random_word 
 
 
 
+def password_length_generator(password_length, list_of_words):
+
+    password_list = []
+    for i in range(password_length) :
+
+        password_list.append(password_generator(words_to_use_for_password_gen))
+
+    return "".join(password_list)
+
+    
+
+    
 
 if __name__ == "__main__":
     
     words_to_use_for_password_gen = text_grabber("FiveLetterWordsGood.txt")
    
+    print(password_length_generator(4,words_to_use_for_password_gen))
+
+
+    
+ 
+ 
+
